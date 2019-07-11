8e5abfb71357aef7b1ed10e4a5dae606faa72f39
kmiku7 <kakoimiku@gmail.com>
2019-07-11 18:36:00 +0800

    add file backend implement.

---
+++ filebackend.go
@@ -0,0 +1,253 @@
+package golog
+
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path"
+	"path/filepath"
+	"regexp"
+	"strings"
+	"sync"
+	"time"
+)
+
+const (
+	defaultFlushInterval = time.Second * 3
+	defaultBufferSize    = 256 * 1024
+	datetimeSuffixLayout = "2006010215"
+)
+
+var (
+	rotatedFilenamePattern *regexp.Regexp
+)
+
+func init() {
+	regexp.MustCompile("(INFO|ERROR|WARNING|DEBUG|FATAL)\\.log\\.20[0-9]{8}")
+}
+
+func truncateToHour(t time.Time) time.Time {
+	return t.Truncate(time.Hour)
+}
+
+type syncBufio struct {
+	writer    *bufio.Writer
+	file      *os.File
+	writeSize uint64
+	filePath  string
+}
+
+func newSyncBufio(file *os.File, filepath string, bufferSize int) *syncBufio {
+	return &syncBufio{
+		writer:   bufio.NewWriterSize(file, bufferSize),
+		file:     file,
+		filePath: filepath,
+	}
+}
+
+func (s *syncBufio) flush() error {
+	return s.writer.Flush()
+}
+
+func (s *syncBufio) sync() error {
+	return s.file.Sync()
+}
+
+func (s *syncBufio) close() error {
+	if err := s.flush(); err != nil {
+		fmt.Fprintf(os.Stderr, "flush failed: %v", err)
+	}
+	if err := s.sync(); err != nil {
+		fmt.Fprintf(os.Stderr, "sync failed: %v", err)
+	}
+	return s.file.Close()
+}
+
+func (s *syncBufio) write(content []byte) {
+	writeCount, err := s.writer.Write(content)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "write file failed: %v", err)
+	}
+	s.writeSize += uint64(writeCount)
+}
+
+type FileBackend struct {
+	mutex          sync.Mutex
+	dir            string
+	writer         [levelCount]*syncBufio
+	flushInterval  time.Duration
+	rotateByHour   bool
+	lastRotateTime int64
+	keepHours      int
+
+	rotatedFilenamePattern *regexp.Regexp
+	getNowTime             func() time.Time
+}
+
+func NewFileBackend(dir string) (*FileBackend, error) {
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		return nil, err
+	}
+	var fileBackend FileBackend
+	fileBackend.dir = dir
+	fileBackend.flushInterval = defaultFlushInterval
+	fileBackend.rotatedFilenamePattern = rotatedFilenamePattern
+	fileBackend.getNowTime = time.Now
+
+	for i := 0; i < int(levelCount); i++ {
+		filepath := path.Join(dir, levelNames[i]+".log")
+		file, err := os.OpenFile(filepath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			return nil, err
+		}
+		fileBackend.writer[i] = newSyncBufio(file, filepath, defaultBufferSize)
+	}
+
+	intervalLoop := func(f func(), d time.Duration) {
+		for {
+			time.Sleep(d)
+			f()
+		}
+	}
+
+	go intervalLoop((&fileBackend).doFlush, fileBackend.flushInterval)
+	go intervalLoop((&fileBackend).doMonitorFiles, time.Second*5)
+	go intervalLoop((&fileBackend).doRotateByHour, time.Second*1)
+
+	return &fileBackend, nil
+}
+
+func (s *FileBackend) SetRotateFile(rotateByHour bool, keepHours int) {
+	s.rotateByHour = rotateByHour
+	if rotateByHour {
+		s.keepHours = keepHours
+		s.lastRotateTime = truncateToHour(s.getNowTime()).Unix()
+	} else {
+		s.lastRotateTime = 0
+	}
+}
+
+func (s *FileBackend) SetFlushInterval(t time.Duration) {
+	s.flushInterval = t
+}
+
+func (s *FileBackend) doFlush() {
+	s.Flush()
+}
+
+func (s *FileBackend) doRotateByHour() {
+	if !s.rotateByHour {
+		return
+	}
+
+	// rotate files
+	rotateTime := truncateToHour(s.getNowTime())
+	if rotateTime.Unix() > s.lastRotateTime {
+		for i := 0; i < int(levelCount); i++ {
+			originalFilename := s.writer[i].filePath
+			newFilename := originalFilename + rotateTime.Format(datetimeSuffixLayout)
+			os.Rename(originalFilename, newFilename)
+		}
+	}
+
+	// remove old files
+	if s.keepHours <= 0 {
+		return
+	}
+	files, err := ioutil.ReadDir(s.dir)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "read dir %s failed: %v", s.dir, err)
+		return
+	}
+	for _, file := range files {
+		if file.Name() == s.rotatedFilenamePattern.FindString(file.Name()) &&
+			s.shouldDelete(file.Name(), s.keepHours) {
+			fullpath := filepath.Join(s.dir, file.Name())
+			if err := os.Remove(fullpath); err != nil {
+				fmt.Fprintf(os.Stderr, "remove %s failed: %v", fullpath, err)
+			}
+		}
+	}
+}
+
+func (s *FileBackend) doMonitorFiles() {
+	for i := 0; i < int(levelCount); i++ {
+		writer := s.writer[i]
+		filepath := writer.filePath
+		_, err := os.Stat(filepath)
+		if err == nil {
+			return
+		}
+		if !os.IsNotExist(err) {
+			fmt.Fprintf(os.Stderr, "stat %s failed: %v", filepath, err)
+			return
+		}
+
+		s.mutex.Lock()
+		defer s.mutex.Unlock()
+		file, err := os.OpenFile(filepath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "open %s failed: %v", filepath, err)
+			return
+		}
+		writer.close()
+		s.writer[i] = newSyncBufio(file, filepath, defaultBufferSize)
+	}
+}
+
+func (s *FileBackend) flush() {
+	for i := 0; i < int(levelCount); i++ {
+		s.writer[i].flush()
+		s.writer[i].sync()
+	}
+}
+
+func (s *FileBackend) Flush() {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+	s.flush()
+}
+
+func (s *FileBackend) close() {
+	for i := 0; i < int(levelCount); i++ {
+		if err := s.writer[i].close(); err != nil {
+			fmt.Fprintf(os.Stderr, "close failed: %v", err)
+		}
+		s.writer[i] = nil
+	}
+}
+
+func (s *FileBackend) Close() {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+	s.close()
+}
+
+func (s *FileBackend) shouldDelete(name string, keepHours int) bool {
+	datetimeSuffix := strings.Split(name, ".")[2]
+	fileTime, err := time.Parse(datetimeSuffixLayout, datetimeSuffix)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "parse datetime suffix failed, name: %v, err: %v", name, err)
+		return false
+	}
+	fileTime.Add(time.Duration(keepHours) * time.Hour)
+	removePoint := truncateToHour(s.getNowTime())
+	if fileTime.Before(removePoint) {
+		return true
+	}
+	return false
+}
+
+func (s *FileBackend) Log(level Level, content []byte) {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+	if level >= 0 && level < levelCount {
+		s.writer[level].write(content)
+	} else {
+		fmt.Fprintf(os.Stderr, "invalid level: %v, content: %s", level, content)
+	}
+	if level == Fatal {
+		s.flush()
+	}
+}
